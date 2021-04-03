bef7430d49eef32df8983b6cbfcd5b55cf9b07e2
Eighthjouster <8j@jscause.org>
2021-04-03 13:01:05 -0700

    Decompressed source.

--- package.sh
+++ package.sh
@@ -1,15 +1,17 @@
 #!/usr/bin/env -S bash ../.port_include.sh
-port=nodejs
-version=serenity-git
-workdir=nodejs-main-serenity
+port=node
+version=v14.16.0
 useconfigure=true
 files="https://nodejs.org/dist/v14.16.0/node-v14.16.0.tar.gz node-v14.16.0.tar.gz"
-configopts="-DCMAKE_TOOLCHAIN_FILE=$SERENITY_ROOT/Toolchain/CMakeToolchain.txt"
+#configopts="--disable-nls --without-bash-malloc"
 
-configure() {
-    run cmake $configopts
+build() {
+#    run_replace_in_file "s/define GETCWD_BROKEN 1/undef GETCWD_BROKEN/" config.h
+#    run_replace_in_file "s/define CAN_REDEFINE_GETENV 1/undef CAN_REDEFINE_GETENV/" config.h
+    run make $makeopts
 }
 
-install() {
-    run make install
+post_install() {
+    mkdir -p "${SERENITY_BUILD_DIR}/Root/bin"
+    ln -s /usr/local/bin/node "${SERENITY_BUILD_DIR}/Root/bin/node"
 }
